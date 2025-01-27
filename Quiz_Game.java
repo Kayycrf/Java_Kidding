3a732f09e1280743d37cbcd7897f51f4a14487e4
Kayycrf <134874690+Kayycrf@users.noreply.github.com>
2025-01-27 11:16:02 -0300

    Update and rename App.java to Quiz_Game.java

--- Quiz_Game.java
+++ Quiz_Game.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App {
+public class Quiz_Game {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         int score = 0;
